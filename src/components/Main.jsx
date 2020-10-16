e02371ff406755ac02d89d671e15b608fc7d1309
hajar nasr <hajar-nasr@hotmail.com>
2020-10-16 14:41:02 +0200

    Exercise 10.5: polished reviewed repositories list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,6 +6,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    paddingBottom:90
   }
 });
 
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,32 +1,108 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const RepositoryItem = ({item})=>{
-    return(
-        <View>
-           <View>
-               <Text>FullName: {item.fullName}</Text>
-           </View>
-           <View>
-               <Text>Description: {item.description}</Text>
-           </View>
-           <View>
-               <Text>Language: {item.language}</Text>
-           </View>
-           <View>
-               <Text>Stars: {item.stargazersCount}</Text>
-           </View>
-           <View>
-               <Text>Forks: {item.forksCount}</Text>
-           </View>
-           <View>
-               <Text>Reviews: {item.reviewCount}</Text>
-           </View>
-           <View>
-               <Text>Rating: {item.ratingAverage}</Text>
-           </View>
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+  /*   borderStyle:"solid",
+    borderWidth:1,
+    borderColor:"red", */
+    backgroundColor:"whitesmoke"
+  },
+  itemRanks: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  itemHeader: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+  },
+  decContainer: {
+    padding: 5,
+    flex:1,
+  },
+  languageContanier: {
+    alignSelf: 'flex-start'
+  },
+  language: {
+    padding: 6,
+    backgroundColor: "teal",
+    color:"whitesmoke",
+    marginTop:7,
+    borderRadius:5,
+  },
+  fullName:{
+    fontWeight:"bold",
+  },
+  textCenter:{
+    textAlign:"center",
+  }
+});
+const RepositoryItem = ({ item }) => {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.itemHeader}>
+        <Image source={{ uri: item.ownerAvatarUrl }} style={styles.avatar} />
+        <View style={styles.decContainer}>
+          <View>
+            <Text style={styles.fullName}>{item.fullName}</Text>
+          </View>
+          <View>
+            <Text>{item.description}</Text>
+          </View>
+          <View style={styles.languageContanier}>
+            <Text style={styles.language}>{item.language}</Text>
+         </View>
         </View>
-    )
-}
+      </View>
+
+      <View style={styles.itemRanks}>
+          <CustomText
+               title ="Stars"
+               value = {item.stargazersCount}
+          />
+          <CustomText
+               title ="Forks"
+               value = {item.forksCount}
+          />
+          <CustomText
+               title ="Reviews"
+               value = {item.reviewCount}
+          />
+          <CustomText
+               title ="Rating"
+               value = {item.ratingAverage}
+          />
+
+      </View>
+    </View>
+  );
+};
 
 export default RepositoryItem;
+
+
+const CustomText = ({title, value})=>(
+  <View>
+    <View>
+      <Text style={styles.textCenter}>{roundNum(value)}</Text>
+    </View>
+    <View>
+      <Text style={styles.textCenter}>{title}</Text>
+    </View>
+  </View>
+)
+
+const roundNum = num =>(
+  num >= 1000?
+    `${parseFloat((num/1000).toFixed(1))}k`
+  :
+  num
+);
